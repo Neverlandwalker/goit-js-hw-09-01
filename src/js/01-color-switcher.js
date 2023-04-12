85d45778a2db5c8fefc4a7ba47e4a2bac6946f17
Neverlandwalker <Blonnyya@gmail.com>
2023-04-12 14:26:18 +0300

    1st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector(["[data-start]"]);
+const stoptBtn = document.querySelector(["[data-stop]"]);
+const bodyBg = document.querySelector('body')
 
+startBtn.addEventListener('click', onStartClick);
+stoptBtn.addEventListener('click', onStopClick);
+
+let bodyColorChangeInterval = null;
+startBtn.disabled = false;
+
+function onStartClick(evt) {
+    bodyColorChangeInterval = setInterval(() => {
+        bodyBg.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+    startBtn.disabled = true;
+    
+}
+function onStopClick(evt) {
+    clearInterval(bodyColorChangeInterval)
+    startBtn.disabled = false;
+    bodyBg.style.backgroundColor = 'white';
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
